c28046526b1fa3178f696e56d62db5344f4267ba
Will Scott <willscott@gmail.com>
2014-02-22 15:39:53 -0800

    change map style & vis

--- src/script.js
+++ src/script.js
@@ -1,13 +1,12 @@
 $(function() {
-    var osm = L.tileLayer.grayscale('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    });
-
     var map = L.map('map', {
         center: [47.6210, -122.3328],
-        zoom: 13,
-        layers: [osm]
+        zoom: 13
     });
+
+    var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/domoritz.h6ibh733/{z}/{x}/{y}.png', {
+      attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+    }).addTo(map);
     
     var popup;
     
@@ -21,24 +20,30 @@ $(function() {
 
     var heatmapLayer = L.TileLayer.heatMap({
         radius: { value: 100, absolute: true },
-        opacity: 0.9,
+        opacity: 1.0,
         gradient: {
           0.0: "rgb(0,255,0)",
-          0.45: "rgba(0,255,0, 0)",
-          0.50: "rgba(255,255,0,0)",
+          0.45: "rgba(0,255,0, 1)",
+          0.50: "rgba(255,255,0,1)",
           0.55: "rgb(255,255,0)",
           1.0: "rgb(255,0,0)"
         }
     });
 
     $.getJSON('data/heatmap.json', function(data) {
+        var max = 2;
+        var min = -1;
+
         var heatmapData = [];
         $.each(data, function(ind, pt) {
           // pt.value = pt.delta;
+          
           var stop = {};
           stop.lat = pt.lat;
           stop.lon = pt.lon;
-          stop.deltaT = (pt.proposed - pt.current) / pt.current;
+          var delta = 1 + (pt.proposed - pt.current) / pt.current;
+          stop.value = (delta - min)/(max - min);
+          
           console.log(stop);
           heatmapData.push(stop);
         });

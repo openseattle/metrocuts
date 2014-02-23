2c390729d17bc9d42768cd2bd781783bfc550a6b
Will Scott <willscott@gmail.com>
2014-02-22 16:01:49 -0800

    show stuff

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 $(function() {
     var map = L.map('map', {
         center: [47.6210, -122.3328],
-        zoom: 13
+        zoom: 10
     });
 
     var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/domoritz.h6ibh733/{z}/{x}/{y}.png', {
@@ -18,37 +18,50 @@ $(function() {
       popup.setContent("hi!" + e.latlng).openOn(map);
     }); 
 
-    var heatmapLayer = L.TileLayer.heatMap({
+    var delayHeatmap = L.TileLayer.heatMap({
         radius: { value: 100, absolute: true },
         opacity: 1.0,
         gradient: {
-          0.0: "rgb(0,255,0)",
-          0.45: "rgba(0,255,0, 1)",
-          0.50: "rgba(255,255,0,1)",
-          0.55: "rgb(255,255,0)",
+          0.0: "rgba(255,0,0,0.5)",
           1.0: "rgb(255,0,0)"
         }
     });
+    var betterHeatmap = L.TileLayer.heatMap({
+        radius: { value: 100, absolute: true },
+        opacity: 1.0,
+        gradient: {
+          0.0: "rgba(0,255,0,0.5)",
+          1.0: "rgb(0,255,0)"
+        }
+    });
 
-    $.getJSON('data/heatmap.json', function(data) {
+    $.getJSON('data/stops_data.json', function(data) {
         var max = 2;
         var min = -1;
 
-        var heatmapData = [];
+        var dheatmapData = [];
+        var gheatmapData = [];
         $.each(data, function(ind, pt) {
           // pt.value = pt.delta;
           
           var stop = {};
-          stop.lat = pt.lat;
-          stop.lon = pt.lon;
-          var delta = 1 + (pt.proposed - pt.current) / pt.current;
-          stop.value = (delta - min)/(max - min);
+          stop.lat = pt.Lat;
+          stop.lon = pt.Long;
+//          var delta = 1 + (pt.proposed - pt.current) / pt.current;
+//          stop.value = (delta - min)/(max - min);
+          stop.value = 2 * Math.random() - 1;
           
-          console.log(stop);
-          heatmapData.push(stop);
+          if (stop.value > 0) {
+            dheatmapData.push(stop);            
+          } else {
+            stop.value = -1 * stop.value;
+            gheatmapData.push(stop);                        
+          }
         });
-        heatmapLayer.setData(heatmapData);
-        heatmapLayer.addTo(map);
+        betterHeatmap.setData(gheatmapData);
+        betterHeatmap.addTo(map);
+        delayHeatmap.setData(dheatmapData);
+        delayHeatmap.addTo(map);
     });
 
     var hash = new L.Hash(map);
